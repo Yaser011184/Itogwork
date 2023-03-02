081dfa4abcb28861121fc9f117c5aadd4071968c
Sergei <yagelavichus@gmail.com>
2023-03-02 21:59:01 +0200

    add Code C#

---
+++ Code C#/Program.cs
@@ -0,0 +1,13 @@
+﻿// Код для решения задачи на скалярное произведение на языке C#
+
+int [] firstVector = {1 , 8};
+int [] secondVector = {6 , 3};
+int size = 2;
+int scalarProdact = 0;
+
+for (int index = 0; index < size; index++)
+{
+    scalarProdact = scalarProdact + firstVector[index] * secondVector[index];
+}
+
+Console.WriteLine(scalarProdact);
